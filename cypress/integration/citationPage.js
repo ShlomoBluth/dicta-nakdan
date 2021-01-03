35b2c2edf7d3b3455bad7ee7e1ec47c6f115affe
Shlomo Bluth <lomibluth@gmail.com>
2021-01-03 12:35:06 +0200

    RequestTests

--- cypress/integration/citationPage.js
+++ cypress/integration/citationPage.js
@@ -5,17 +5,17 @@
 describe('NakdanPage',()=>{
   
     beforeEach(() => {
-      cy.visit('https://nakdan.dicta.org.il/')
+      cy.visit('https://citation.dicta.org.il/')
     })
 
     it('Page is not stuck after Request failed with status code 500 when clicking the ron butten',()=>{
         cy.intercept('POST', '/api', {
             statusCode: 500
         })
-        cy.get('textarea').type(' משה קיבל תורה מסיניי, ומסרה ליהושוע, ויהושוע לזקנים') 
-        cy.get('ul[class*="accordion-list"]>li').each(($el)=>{
-            cy.get($el).children().click().then(()=>{
-                cy.get('[type="button"]').click()
+        cy.get('textarea[id="textEntryArea"]').type('משה קיבל תורה')
+        cy.get('[id="findInstancesBttn"]').click().then(()=>{
+            cy.get('.modal-body').within(()=>{
+                cy.get('span').invoke('text').should('contain','לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר')
             })
         })
     })
---
+++ cypress/integration/requestsTests.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress"/>
+
+//run tests on requests from nakdan run
+
+describe('requestsTests',()=>{
+
+    
+    beforeEach(() => {
+      cy.visit('https://nakdan.dicta.org.il/')
+    })
+
+    it('Successful request after request with 60 seconds delay of response when clicking the run'+
+    ' butten of nakdan page '
+    ,()=>{
+        cy.nakdanRequest({
+          delaySeconds:60})
+     })
+
+    it('Message after request failed with 61 seconds delay of response when clicking the ron butten'+
+    ' of nakdan page'
+    ,()=>{
+        cy.nakdanRequest({
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+          delaySeconds:61
+        })
+    })
+
+  
+   it('Message after request failed with status code 500 when clicking the ron butten of nakdan page'
+    ,()=>{
+        cy.nakdanRequest({
+          status:500,
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+        })
+    })
+    
+})
