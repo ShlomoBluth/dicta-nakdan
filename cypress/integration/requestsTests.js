e6390bcfee5284391142fc23d0a56f2ac62dbce2
Shlomo Bluth <lomibluth@gmail.com>
2021-01-06 08:07:20 +0200

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -5,27 +5,26 @@
 describe('requestsTests',()=>{
 
     
-    beforeEach(() => {
-      cy.visit('https://nakdan.dicta.org.il/')
-    })
+  beforeEach(() => {
+    cy.visit('https://nakdan.dicta.org.il/')
+  })
 
 
-    it('Message after request with 2 minutes delay of response failed when clicking the ron butten'+
-    ' of nakdan page'
-    ,()=>{
-        cy.nakdanRequest({
-          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-          delaySeconds:60*2
-        })
+  it('Error message for response with a delay of 2 minutes when clicking the run butten'+
+  ' of nakdan page',()=>{
+    cy.nakdanRequest({
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+      delaySeconds:60*2
     })
+  })
 
   
-   it('Message after request with status code 500 failed when clicking the ron butten of nakdan page'
-    ,()=>{
-        cy.nakdanRequest({
-          status:500,
-          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
-        })
+  it('Error message for response with status code 500 when clicking the run butten of nakdan page'
+  ,()=>{
+    cy.nakdanRequest({
+      status:500,
+      message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
     })
+  })
     
 })
