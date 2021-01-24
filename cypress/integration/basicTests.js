bbcdf67c6b6fde98db6c34467e99a4a6dad0d27c
Shlomo Bluth <lomibluth@gmail.com>
2021-01-24 15:23:57 +0200

    basicTests

---
+++ cypress/integration/basicTests.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+//run basic tests on nakdan run
+
+describe('basicTests',()=>{
+
+    beforeEach(() => {
+        cy.visit('https://nakdan.dicta.org.il/')
+      })
+    
+    it('Modern nakdan',()=>{
+        cy.get('input[id="use-modern-nakdan"]').click({force: true})
+        cy.runNakdan('משה קיבל תורה מסיני')
+        cy.resultsTests('מֹשֶׁה קִ⁠בֵּל תּוֹרָה מִסִּינַי')
+    })
+
+    it('Rabbinic nakdan',()=>{
+        cy.get('input[id="use-rabbinic-nakdan"]').click({force: true})
+        cy.runNakdan('משה קיבל תורה מסיני')
+        cy.resultsTests('מֹשֶׁה קִ⁠בֵּל תּוֹרָה מִסִּינַי')
+    })
+
+    it('Poetry nakdan',()=>{
+        cy.get('input[id="use-poetry-nakdan"]').click({force: true})
+        cy.runNakdan('משה קיבל תורה מסיני')
+        cy.resultsTests('משֶׁה קִ⁠בֵּל תּוֹרָה מִסִּינַי')
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,34 @@
 
+Cypress.Commands.add('runNakdan',(text)=>{
+  cy.get('[placeholder="הזן טקסט כאן"]').type(text)
+  cy.get('div[class="run-button"]').within(()=>{
+    cy.get('button').click()
+  })
+})
 
+Cypress.Commands.add('resultsTests',(text)=>{
+  let nakdanResults=''
+  cy.get('[id*="span-C-word"]').each(($word)=>{
+      cy.get($word).each($letter=>{
+          nakdanResults=nakdanResults+$letter.text()
+      }).then(()=>{
+          nakdanResults=nakdanResults+' ' 
+      })
+  }).then(()=>{
+      expect(nakdanResults.substring(0,nakdanResults.length-1))
+      .to.eq(text)
+  }) 
+})
 
 Cypress.Commands.add('nakdanRequest',({url,status=200,message='',delaySeconds=0})=>{
   cy.intercept( 'POST', '/'+url, {
     delayMs:1000*delaySeconds,
     statusCode: status
   })
-  cy.get('[placeholder="הזן טקסט כאן"]').type('משה קיבל תורה')
   if(message.length>0){
     cy.contains(message).should('not.exist')
   }
-  cy.get('div[class="run-button"]').within(()=>{
-      cy.get('button').click()
-  })
+  cy.runNakdan('משה קיבל תורה')
 
   if(delaySeconds>0){
     cy.get('[class*="spinner"]').should('exist')
